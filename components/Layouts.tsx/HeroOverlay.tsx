722c2ee43d70bf4ccaa48f943d8abe976f0a5e97
morfeli <fmoreira95@hotmail.com>
2023-03-08 19:48:44 -0500

    fix: small tweak test

--- components/Layouts.tsx/HeroOverlay.tsx
+++ components/Layouts.tsx/HeroOverlay.tsx
@@ -9,7 +9,7 @@ export const HeroOverlay = ({ children, id }: HeroOverlayProps) => {
       className="relative flex flex-col items-center justify-center w-screen h-[900px] lg:h-screen overflow-hidden text-white bg-gradient-to-t from-greenSecondary to-greenPrimary"
       id={id && id}
     >
-      <div className="w-[1000px] h-[1000px] bg-bluePrimary blur-[200px] rounded-full absolute left-[35%] top-[-100px] " />
+      <div className="w-[1000px] h-[1000px] bg-red-800 blur-[200px] rounded-full absolute left-[35%] top-[-100px] " />
       <div className="z-50 flex flex-col justify-between pt-8 sm:px-4">
         {children}
       </div>
