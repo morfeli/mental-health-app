734957c2456b2abca837e63dbcd543dcbe75d311
morfeli <fmoreira95@hotmail.com>
2023-04-18 16:01:52 -0400

    new: donor wall page

--- components/DonatePage/DonatePage.tsx
+++ components/DonatePage/DonatePage.tsx
@@ -4,7 +4,7 @@ export const DonatePage = () => {
   return (
     <section className="relative z-50 pb-16 bg-white 2xl:h-screen pt-28 xl:pt-20 xl:flex xl:justify-around bg-gradient-to-br from-sky-400 to-cyan-300 ">
       <div className="p-4 pl-5 text-white xl:pt-20">
-        <h1 className="z-50 pb-2 text-5xl md:text-7xl">DONATE TODAY</h1>
+        <h1 className="z-50 pb-2 text-5xl md:text-7xl">test</h1>
         <h2 className="pb-4 text-4xl md:text-4xl xl:py-6">
           Be a part of the change
         </h2>
